3131302064d2603a2845f436e17f19a3e126b50f
Arshad Khan <arshad18091999@gmail.com>
2025-04-22 18:33:57 +0530

    Initial commit

---
+++ app/src/main/java/com/nexachats/miniapp/application/MiniApp.kt
@@ -0,0 +1,12 @@
+package com.nexachats.miniapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @Created by Arshad Khan on 4/22/2025.
+ * Test
+ * Test 2
+ */
+@HiltAndroidApp
+class MiniApp : Application()
